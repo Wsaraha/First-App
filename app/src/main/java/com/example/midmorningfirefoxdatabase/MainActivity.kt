022795fda558568e4928fe4b9151365df6c8cde1
Wsaraha <hassansaraha3@gmail.com>
2023-04-13 13:23:35 +0300

    first commit

---
+++ app/src/main/java/com/example/midmorningfirefoxdatabase/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.midmorningfirefoxdatabase
+
+import android.app.ProgressDialog
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.ContactsContract.CommonDataKinds.Email
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ListView
+import androidx.appcompat.app.AlertDialog
+
+class MainActivity : AppCompatActivity() {
+    lateinit var edtName: EditText
+    lateinit var edtEmail: EditText
+    lateinit var edtIdNumber: EditText
+    lateinit var btnSave: Button
+    lateinit var btnView: Button
+    lateinit var progressDialog: ProgressDialog
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        edtName = findViewById(R.id.mEdtName)
+        edtEmail = findViewById(R.id.mEdtEmail)
+        edtIdNumber = findViewById(R.id.mEdtNumber)
+        btnSave = findViewById(R.id.mBtnSave)
+        btnView = findViewById(R.id.mBtnView)
+        progressDialog = ProgressDialog(this)
+        progressDialog.setTitle("Saving")
+        progressDialog.setMessage("Please wait...")
+        btnSave.setOnClickListener {
+
+        }
+    }
+}
